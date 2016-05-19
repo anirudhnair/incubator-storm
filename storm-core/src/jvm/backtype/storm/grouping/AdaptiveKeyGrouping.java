069312887b0eb7a272f786c612379f716898fb4f
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2016-05-19 17:22:12 -0500

    adaptive key grouping and wiring up task ratio setting in executors

---
+++ storm-core/src/jvm/backtype/storm/grouping/AdaptiveKeyGrouping.java
@@ -0,0 +1,7 @@
+package backtype.storm.grouping;
+
+/**
+ * Created by anirudhnair on 5/19/16.
+ */
+public class AdaptiveKeyGrouping {
+}
---
+++ storm-core/src/jvm/backtype/storm/grouping/OutCompLoad.java
@@ -0,0 +1,13 @@
+package backtype.storm.grouping;
+
+import java.util.HashMap;
+
+/**
+ * Created by anirudhnair on 5/12/16.
+ */
+public class OutCompLoad implements java.io.Serializable {
+    private String sSrcCompID;
+    private String sDestCompID;
+
+    private HashMap<Integer,Float> load
+}
