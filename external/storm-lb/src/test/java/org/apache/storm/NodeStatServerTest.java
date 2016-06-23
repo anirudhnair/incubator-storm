47968835cd7e4adedd8633f74637581a9a34b22f
Anirudh Jayakumar <jayakumar.anirudh@gmail.com>
2016-06-23 11:37:53 -0700

    test for node stat

---
+++ external/storm-lb/src/test/java/org/apache/storm/NodeStatServerTest.java
@@ -0,0 +1,32 @@
+package org.apache.storm;
+
+import org.apache.thrift.TException;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by anirudhnair on 6/23/16.
+ */
+public class NodeStatServerTest {
+
+    public static void main( String[] args ) throws InterruptedException, TException {
+        String sIP = args[0];
+        Logger logger = new Logger();
+        String timestamp = new SimpleDateFormat("HH:mm:ss:SSS").format(Calendar.getInstance().getTime());
+        logger.Initialize("/home/ajayaku2/log/NodeStatTest_" + timestamp + ".log");
+        NodeStatClient client = new NodeStatClient();
+        client.Init(sIP, logger);
+        while(true)
+        {
+            System.out.println(client.CPUUsage());
+            System.out.println(client.MemUsage());
+            System.out.println(client.PowerUsgae());
+            System.out.println("-----------------");
+            Thread.sleep(10000);
+        }
+        System.exit(Common.SUCCESS);
+    }
+}
